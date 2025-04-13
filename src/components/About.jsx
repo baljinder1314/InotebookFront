6a0aee1136566ecabce808f86d67633c23125b40
Baljinder-Singh <baljindersingh987209@gmail.com>
2025-04-13 19:02:18 +0530

    refactor: improve layout and styling in About component; adjust class names and fix minor formatting issues

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,29 +6,29 @@ function About() {
   let navigate = useNavigate();
   const { responseData, hanldeLogout } = useContext(Store); // Access responseData from context
 
-  if(responseData === null){
-    return ;
+  if (responseData === null) {
+    return;
   }
 
   return (
     <div className="absolute w-full inset-0 mt-10 z-10 flex justify-center items-center">
-      <div className="border border-white py-6 px-8 w-full  max-w-xl flex justify-center items-center flex-col space-y-5 shadow-lg rounded-lg @container">
-        <div className="@2xs:flex  justify-center items-center space-x-4">
-          <div className="rounded-4xl  min-w-10 w-50 ">
+      <div className="border border-white py-6 px-8 w-full max-w-xl flex justify-center items-center flex-col space-y-5 shadow-lg rounded-lg @container">
+        <div className="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-4">
+          <div className="rounded-4xl min-w-10 w-32 sm:w-50">
             <img
-              className="min-w-10 w-50 rounded-4xl"
+              className="min-w-10 px-5 w-32 sm:w-50 rounded-4xl"
               src={responseData.localPhoto}
               alt=""
             />
           </div>
-          <div className=" space-y-1 sm:space-y-2">
-            <p className="text-2xl font-semibold  text-white">Profile</p>
+          <div className="space-y-2 sm:space-y-4 text-center sm:text-left">
+            <p className="text-xl sm:text-2xl font-semibold text-white">Profile</p>
             {responseData ? (
               <>
-                <p className="text-base sm:text-2xl text-white">
+                <p className="text-sm sm:text-base lg:text-xl text-white">
                   <strong>Name:</strong> {responseData.localUsername}
                 </p>
-                <p className="text-base sm:text-2xl text-white">
+                <p className="text-sm sm:text-base lg:text-xl text-white">
                   <strong>Email:</strong> {responseData.localEmail}
                 </p>
               </>
@@ -40,7 +40,7 @@ function About() {
           </div>
         </div>
         <button
-          className="px-4 py-2 w-full font-semibold bg-blue-500 sm:text-2xl text-white rounded hover:bg-blue-600 transition"
+          className="px-4 py-2 w-full font-semibold bg-blue-500 text-sm sm:text-base lg:text-xl text-white rounded hover:bg-blue-600 transition"
           onClick={() => hanldeLogout(navigate)}
         >
           Logout
