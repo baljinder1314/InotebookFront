89df1e4bd8767cded4641013759cb9523e90bbec
Baljinder-Singh <baljindersingh987209@gmail.com>
2025-04-17 13:37:05 +0530

    fix: update background image path in Login and Register components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -21,7 +21,7 @@ function Login() {
 
   return (
     <>
-      <div className="relative h-screen bg-[url(wallhaven-x13lxo_1920x1080.png)] bg-center bg-cover bg-no-repeat">
+      <div className="relative h-screen bg-[url(../public/wallhaven-x13lxo_1920x1080.png)] bg-center bg-cover bg-no-repeat">
         {/* Overlay */}
         <div className="absolute inset-0 bg-black opacity-66"></div>
         {/* Form */}
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -61,7 +61,7 @@ function Register() {
 
   return (
     <>
-      <div className="relative h-screen bg-[url(wallhaven-x13lxo_1920x1080.png)] bg-center bg-cover bg-no-repeat">
+      <div className="relative h-screen bg-center bg-cover bg-no-repeat bg-[url('../public/wallhaven-x13lxo_1920x1080.png')]">
         {/* Overlay */}
         <div className="absolute inset-0 bg-black opacity-66"></div>
         {/* Form */}
